b2458256d0963b9d47ceb7d49637af1e44611b90
MilesInSpace <Cshank.public@gmail.com>
2022-06-03 18:07:54 -0400

    now displays an image successfully, now reads in google sheet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import driveData from "./gdrive-data.json";
+import ArtSpaceDataLoader from "./ArtSpaceDataLoader.js";
+
 
 function App() {
+  var formattedData = ArtSpaceDataLoader(driveData);
+  console.log("THIS IS FROM APP " + formattedData)
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +24,10 @@ function App() {
         </a>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </header>
+    
+    <ArtSpaceDataLoader data ={driveData}/>
+    <p>eyy again</p>
+    <img src="https://drive.google.com/uc?id=1B4qwC4CO101oqV3efNTYSN84q_oerHqU"/>
     </div>
   );
 }
