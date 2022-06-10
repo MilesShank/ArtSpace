1f9a63333b151f3680dfd34b2edf18291ed1d25e
MilesInSpace <Cshank.public@gmail.com>
2022-06-10 11:34:20 -0400

    6-10 phased out pipedream URL for google sheets call

--- src/App.js
+++ src/App.js
@@ -3,41 +3,57 @@ import "./App.css";
 import driveData from "./gdrive-data.json";
 import { useEffect, useState } from "react";
 import TestComponent from "./components/TestComponent";
+import { data } from "autoprefixer";
 const gDriveImgLink = "https://drive.google.com/uc?id=";
 var imgData;
 const requestOptions = {
   method: 'GET',
   redirect: 'follow'
+
 };
 var imgData;
 
 function App() {
-
-  function loadData() {
-    fetch("https://eoimtcqaziwadc2.m.pipedream.net/", requestOptions)
-    .then(response => response.json())
-    .then(data => setImgData(data))
-    .catch(error => console.log('error', error));
-    };
 
   const [imgData, setImgData] = useState([]);
   useEffect(() => {
     loadData();
   });
 
-    function formatData(responseJson){
-      imgData = responseJson;
-      console.log(imgData[1][0]);
+  async function loadData() {
+    fetch("https://sheets.googleapis.com/v4/spreadsheets/1o41jm0d7qFoIJP8QEC8U70q6rvdLO2RSRnjyLOiy_qk/values/Sheet1"+"?key="+driveData.gkey)
+    .then(response => response.json())
+    .then(data => formatData(data.values))
+    .catch(error => console.log('error', error));
+    };
+
+
+
+    function formatData(dataValues){
+      const imgDataKeys = dataValues[0]
+      console.log("img data keys" + imgDataKeys
+      )
+      console.log("")
+      console.log(dataValues)
+      setImgData(dataValues)
+
+      // console.log("FROM FORMAT DATA" + imgData);
     } 
+
+    function DisplayOneImage(){
+      const imgLink = (gDriveImgLink + imgData);
+        <img src={imgLink}> </img>
+    };
+
   return (
     <div className="App">
       <TestComponent/>
+      <DisplayOneImage/>
         <p>
           {imgData}
-          Edit <code>eyyy</code> and save to reload.
+          <code>eyyy</code>
         </p>
-        <test/>
-    <p>eyy again</p>
+      <p>eyy again</p>
     </div>
      
   );
