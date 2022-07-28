4f1d22854b5e7216bbca52df448a1edb366280b5
MilesInSpace <Cshank.public@gmail.com>
2022-07-28 14:48:27 -0400

    Added routes and navigation to dummy project and about page, as well as default navigation to app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { About } from "./About.js";
 const Header = () => {
   return (
     <div className="header">
       <nav class="bp4-navbar bp4-dark">
         <div class="bp4-navbar-group bp4-align-left">
-          <div class="bp4-navbar-heading">ArtSpace</div>
+          <div class="bp4-navbar-heading">
+            <Link to="/">ArtSpace</Link>
+          </div>
         </div>
         <div class="bp4-navbar-group bp4-align-center">
-          <button class="bp4-button bp4-minimal">Home</button>
-          <button class="bp4-button bp4-minimal">Projects</button>
-          <button class="bp4-button bp4-minimal">About</button>
-          <button class="bp4-button bp4-minimal">Shop</button>
+          <button class="bp4-button bp4-minimal">
+            <Link to="/">Home</Link>
+          </button>
+          <button class="bp4-button bp4-minimal">
+            <Link to="/Projects">Projects</Link>
+          </button>
+          <button class="bp4-button bp4-minimal">
+            <Link to="/About">About</Link>
+          </button>
+          <button class="bp4-button bp4-minimal">
+            <Link to="/Shop">Shop</Link>
+          </button>
         </div>
       </nav>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import About from "./components/About";
+import Projects from "./routes/Projects";
 import Header from "./components/Header";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
-    <About />
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="About" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
